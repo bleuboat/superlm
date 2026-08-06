43d1c2fc9b672d9f810f2f2ab36b484a1db80c41
bleuboat <3751541788@qq.com>
2026-08-07 00:07:00 +0800

    update api

--- run_api.py
+++ run_api.py
@@ -1,16 +1,26 @@
 from flask import Flask, request, Response
+from flask_cors import CORS
 from superlm import WorkSpace
-app = Flask(__name__)
-workspace = WorkSpace('backrooms')
 
-@app.route('/api', methods=['GET'])
-def api():
-    args = dict(request.args)
+app = Flask(__name__)
+CORS(app)
+workspaces = {}
+
+@app.route('/api/<name>', methods=['GET', 'POST'])
+def api(name: str) -> Response:
+    workspace = workspaces.get(name, None)
+    if workspace is None:
+        workspace = WorkSpace(name)
+        workspaces[name] = workspace
+    if request.method == 'GET':
+        args = dict(request.args)
+    else:
+        args = dict(request.get_json())
     prompt = args.pop('prompt', '')
     return Response(
         workspace.api(prompt, **args),
         mimetype='text/event-stream',
-        headers={'Cache-Control': 'no-cache'}
+        headers={'Cache-Control': 'no-cache'},
     )
 
 if __name__ == '__main__':
