1d040e71bdfe6df64f5a6808153f0e23e752b15f
bleuboat <3751541788@qq.com>
2026-08-18 00:08:07 +0800

    完善类型检查

--- run/run.py
+++ run/run.py
@@ -1,6 +1,6 @@
 from superlm import *
-space = WorkSpace('luxun/test', seed=42, dtype=dtypes.bfloat16)
-space.config(model_type='transformer', n_embd=64)
+space = WorkSpace("luxun/test", seed=42, dtype=dtypes.bfloat16)
+space.config(model_type="transformer", n_embd=64)
 space.config(epochs=1, batch_size=2, accumulation_steps=1)
 space.info()
 space.train(length=200)
--- run/run_api.py
+++ run/run_api.py
@@ -7,29 +7,30 @@ from functools import lru_cache
 app = Flask(__name__)
 CORS(app)
 
+
 @lru_cache(maxsize=3)
 def get_workspace(name: str) -> WorkSpace:
     return WorkSpace(name)
 
+
 def sse(x: str) -> str:
     with app.app_context():
-        return f'data: {x}\n\n'
+        return f"data: {x}\n\n"
 
-@app.route('/api/<name>', methods=['GET', 'POST'])
+
+@app.route("/api/<name>", methods=["GET", "POST"])
 def api(name: str) -> Response:
     workspace = get_workspace(name)
-    if request.method == 'GET':
-        args = dict(request.args)
-    else:
-        args = dict(request.get_json())
-    prompt = args.pop('prompt', '')
+    args = dict(request.args if request.method == "GET" else request.get_json())
+    prompt = args.pop("prompt", "")
     for k, v in args.copy().items():
         args[k] = literal_eval(v)
     return Response(
-        workspace.api(prompt, sse, **args),
-        mimetype='text/event-stream',
-        headers={'Cache-Control': 'no-cache'},
+        workspace.api(prompt, map=sse, **args),
+        mimetype="text/event-stream",
+        headers={"Cache-Control": "no-cache"},
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)  # ruff: ignore[flask-debug-true]
